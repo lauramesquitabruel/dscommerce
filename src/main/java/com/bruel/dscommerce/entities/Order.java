5dbecba61392a98edbbc01d4c53c602a3b5e0ec2
lauramesquitabruel <lauramesquitabruel@gmail.com>
2025-01-26 13:16:55 -0300

    first commit

---
+++ src/main/java/com/bruel/dscommerce/entities/Order.java
@@ -0,0 +1,62 @@
+package com.bruel.dscommerce.entities;
+
+import com.bruel.dscommerce.enums.OrderStatus;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name="order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+    private OrderStatus status;
+
+    //mapeamento do relacionamento N:1
+    @ManyToOne
+    @JoinColumn(name="client_id")
+    private User client;
+
+    public Order(){}
+
+    public Order(Long id, Instant moment, OrderStatus status, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+}
---
+++ src/main/java/com/bruel/dscommerce/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.bruel.dscommerce.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED
+}
