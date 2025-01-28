2bf443826b3835a51a0c5f76f1be8fafcd834a81
lauramesquitabruel <lauramesquitabruel@gmail.com>
2025-01-28 11:44:04 -0300

    busca paginada de produtos

--- src/main/java/com/bruel/dscommerce/controllers/ProductController.java
+++ src/main/java/com/bruel/dscommerce/controllers/ProductController.java
@@ -3,11 +3,14 @@ package com.bruel.dscommerce.controllers;
 import com.bruel.dscommerce.dtos.ProductDTO;
 import com.bruel.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 //o que for implentado por essa classe vai responder pela web
@@ -23,6 +26,13 @@ public class ProductController {
     public ProductDTO findById(@PathVariable Long id){
         //os metódos por padrão retornam um Optional em caso de ero
         return productService.findById(id);
+
+    }
+
+    //rota = /products?size=12&page=n&sort=atr
+    @GetMapping()
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return productService.findAll(pageable);
 
     }
 }
--- src/main/java/com/bruel/dscommerce/services/ProductService.java
+++ src/main/java/com/bruel/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.bruel.dscommerce.dtos.ProductDTO;
 import com.bruel.dscommerce.entities.Product;
 import com.bruel.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,7 +20,13 @@ public class ProductService {
     //o service retorna um DTO
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id){
-        Optional<Product> result = productRepository.findById(id);
-        return new ProductDTO(result.get());
+        Product result = productRepository.findById(id).get();
+        return new ProductDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = productRepository.findAll(pageable);
+        return result.map(ProductDTO::new);
     }
 }
