90274f07c8c4e9f2415cf02bec7c8e3bbd74d8e7
lauramesquitabruel <lauramesquitabruel@gmail.com>
2025-02-17 11:21:44 -0300

    adição do UserDetails e GrantedAuthority, não testados

--- src/main/java/com/bruel/dscommerce/entities/Role.java
+++ src/main/java/com/bruel/dscommerce/entities/Role.java
@@ -1,13 +1,15 @@
 package com.bruel.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -30,6 +32,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
